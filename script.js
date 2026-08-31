0299470456a73651e7cbdaff662cfc165fba613c
canamaganda <racel.cana@student.laverdad.edu.ph>
2026-08-31 19:34:33 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+const nameInput = document.querySelector("#nameInput");
+const courseInput = document.querySelector("#courseInput");
+const studentForm = document.querySelector("#studentForm");
+const welcomeMessage = document.querySelector("#welcomeMessage");
+const toggleTheme = document.querySelector("#toggleTheme");
+const myBody = document.querySelector("body");
+
+nameInput.addEventListener("input", () => {
+  welcomeMessage.textContent = "Hello, " + nameInput.value + "!";
+});
+
+studentForm.addEventListene("submit", (event) => {
+  event.preventDefault();
+  welcomeMessage.textContent = "Welcome, " + nameInput.value + "!"; 
+});
+
+toggleTheme.addEventListener("click", () => {
+
+  myBody.classList.toggle("dark");
+});
+
+document.addEventListener("keydown", (event) -> {
+  if (event.key === "Escape") {
+welcomeMessage.textContent = "";
+}
+});
